815259b82819b278474120a0bae65e971ee0c2a9
kaushik-herle <herlekaushik@gmail.com>
2025-02-17 18:24:39 +0530

    This is Locally created Repo and I am Committing it!

---
+++ ArrayOperation.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ArrayOperation{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array Size:");
+        int n = sc.nextInt();
+        int[] a= new int[n];
+        System.out.println("Enter Array Elements:");
+        for(int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        int half = n / 2;
+        int ans1 = a[0];
+        int ans2 = a[half];
+        for(int i = 1; i < half; i++)
+            ans1 = ans1 * a[i];
+        for(int i = half + 1; i < n; i++)
+            ans2 = ans2 * a[i];
+        if(ans1 < ans2){
+            for(int i = 0; i < n / 2; i++){
+                int temp = a[i];
+                a[i] = a[(n - 1) - i];
+                a[(n - 1) - i] = temp;
+            }
+            for (int i = 0; i < n; i++)
+                System.out.print(a[i] + "\t");
+        }
+        else{
+            for(int i = 0; i < n; i++)
+                System.out.print(a[i] + "\t");
+        }
+        sc.close();
+    }
+}
